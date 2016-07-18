71cba2e49009e53bbbac2c6d940741dcec26d616
Patricia <montes.pati@gmail.com>
2016-07-18 14:08:39 -0500

    Improvements

--- patimontes/proyectofinal/js/main.js
+++ patimontes/proyectofinal/js/main.js
@@ -1,5 +1,6 @@
 $(document).on("ready",enviar);
 
+document.getElementById("resultado").style.visibility="hidden";
 
 function enviar(){
 	$("#envio").on("click",datos);
@@ -8,9 +9,32 @@ function enviar(){
 function datos(){
 	mesa = $('#mesa').val();
 	personas = $('#personas').val();
+	
+	$('#pedido').toggle(800);
 
-	document.getElementById("pedido").innerHTML = "<h4>Resumen de pedido</h4><br>"+ "El numero de mesa es "+mesa+"<br>El numero de personas es: "+personas ;
+	var ensaladas = new Array();
+	//recorremos todos los checkbox seleccionados con .each
+	$('input[name="ensalada[]"]:checked').each(function() {
+		//$(this).val() es el valor del checkbox correspondiente
+		ensaladas.push($(this).val());
+	})
 
+	var sandwich = new Array();
+	$('input[name="sandwich[]"]:checked').each(function() {
+		sandwich.push($(this).val());
+	})
 
+	var bebida = new Array();
+	$('input[name="bebida[]"]:checked').each(function() {
+		bebida.push($(this).val());
+	})
+	
 
+	document.getElementById("resultado").style.visibility="initial";
+
+	document.getElementById("resumen").innerHTML = "El numero de mesa es "+mesa+"<br>El numero de personas es: "
+	+personas +"<br>Han pedido las siguientes ensaladas: "+ ensaladas + "<br> Han pedido los sandwiches: " +
+	sandwich + "<br> Han pedido las siguientes bebidas: " +bebida ;
+	
+	
 }
